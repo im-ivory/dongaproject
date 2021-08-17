4b3419ed37496dc54f7c14649823895ac4addaa2
im-ivory <78600276+im-ivory@users.noreply.github.com>
2021-08-18 00:33:57 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-/////////////page01//////////
+//--------------page01--------------
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ease: "power1", duration: 3});
 
-// ScrollTrigger.saveStyles(".mobile, .desktop");
 
-
-ScrollTrigger.matchMedia({
-  
+ScrollTrigger.matchMedia({  
   "(min-width: 768px)": function() {
     const tl = gsap.timeline();
 tl.to('.page01 > .inner > .title', {
@@ -49,11 +46,7 @@ tl.to('.page01 > .inner > .title', {
 
 
 
-
-
-
-
-// ///////page2///////
+//--------------page02--------------
 gsap.from('.page02 > .title', {
     y:100,
     opacity: 0,
@@ -101,7 +94,6 @@ start: "top 100%",
 end: "+=500",
 }
 })
-
 tl2.to(".background_image > .wuhan", {
 opacity:0,
 scrollTrigger: {
@@ -183,6 +175,16 @@ toggleActions: "play none reverse reset",
   end: "+=200",
 }
 })
+tl2.to(".background_image > .confirmed", {
+  opacity:1,
+  scrollTrigger: {
+  trigger: ".article05 > .text",
+  scrub: 1,
+  toggleActions: "play none reverse reset",
+    start: "top 100%",
+    end: "+=200",
+  }
+  })
 tl2.to(".article06 > .page_fadeout", {
 scale: 11,
 scrollTrigger: {
@@ -195,7 +197,7 @@ toggleActions: "play none reverse reset",
 })
 
 
-// ////page04/////
+//--------------page04--------------
 gsap.from('.page04 > .title > h1', {
     y:100,
     opacity: 0,
@@ -209,7 +211,7 @@ gsap.from('.page04 > .title > h1', {
     }
 })
 
-//button
+////확진자 수 버튼//
 
 const btn = document.querySelector("#jsBtn");
 var chartHeight = document.querySelector(".chart_container").clientHeight;
@@ -230,62 +232,61 @@ function increase(){
 $('#jsBtn').click(function () { 
     $(".counter_container").css("display", "flex");
     $('html, body').stop().animate({
-    scrollTop : $('#up').offset().top -400
+    scrollTop : $('#up').offset().top - 400
 }, 5000)
 });
 
-
 btn.addEventListener("click", increase);
 
-// ///// 시계
 
+// //날짜 카운터// 
 
 $('#jsBtn').click(function(){
-    var count0 = 2020;
-    var count1 = 1;
-    var count2 = 1;
+    var year = 2020;
+    var month = 1;
+    var day = 1;
     timeCounter();
     
-       function timeCounter() {
-         id0 = setInterval(count0Fn, 3600); 
-        function count0Fn() {
-          count0++;
-          if (count0 > 2021) {    
-            clearInterval(id0);
-          } else {
-            $(".number").eq(0).text(count0);
-          }
+    function timeCounter() {
+         
+      dateYear = setInterval(changeYear, 3600); 
+      function changeYear() {
+        year++;
+        if (year > 2021) {    
+          clearInterval(dateYear);
+        } else {
+          $(".number").eq(0).text(year);
         }
+      }
     
-        id1 = setInterval(count1Fn, 300);
-        function count1Fn() {
-          count1++;
-          if (count1 > 12) {
-            count1 = 0;
-          } else if (count1 == 9 && count0 == 2021 && count2 == 3){
-              clearInterval(id1);
-          }else {
-            $(".number").eq(1).text(count1);
-          }
+      dateMonth = setInterval(changeMonth, 300);
+      function changeMonth() {
+        month++;
+        if (month > 12) {
+          month = 0;
+        } else if (month == 9 && year == 2021 && day == 3){
+            clearInterval(dateMonth);
+        }else {
+          $(".number").eq(1).text(month);
         }
+      }
     
-        id2 = setInterval(count2Fn, 10);
-    
-        function count2Fn() {
-          count2++;
-          if (count2 > 31) {
-            count2 = 0;
-          }  else if (count1 == 8 && count0 == 2021 && count2 == 3){
-            clearInterval(id2);
+        dateDay = setInterval(changeDay, 10);
+        function changeDay() {
+          day++;
+          if (day > 31) {
+            day = 0;
+          }  else if (month == 8 && year == 2021 && day == 3){
+            clearInterval(dateDay);
           }else{
-            $(".number").eq(2).text(count2);
+            $(".number").eq(2).text(day);
           }
       }
     }
 });
 
 
-// //// 라인차트
+// //누적 확진자 수 라인 그래프// 
 
 var margin = {top: 30, right: 20, bottom: 100, left: 150},
     width = 1000 - margin.left - margin.right,
@@ -302,14 +303,6 @@ var xAxis = d3.axisBottom(x)
 var yAxis = d3.axisLeft(y)
     .ticks(5);
 
-    // if(window.matchMedia("(min-width: 768px)").matches){
-    //   var yAxis = d3.axisLeft(y)
-    //         .ticks(10);
-    // }else{
-    //   var yAxis = d3.axisLeft(y)
-    //         .ticks(5);
-    // }
-
 var valueline = d3.line()
     .x(function(d) { return x(d.date); })
     .y(function(d) { return y(d.case); });
@@ -320,22 +313,19 @@ var lineTooltip = d3.select("body .page04 .case_line").append("div")
 
 var lineDsc = d3.select("body .page04 .case_line").append("div")
     .attr("class", "line_dsc")
-    .html("<p>대한민국 누적확진자 수</p><p>누적 확진자수(명)</p>")
-
+    .html("<p>대한민국 누적 확진자 수</p><p>누적 확진자 수(명)</p>")
 
 var svg = d3.select("body .page04 .case_line")
     .append("svg")
-          .attr("viewBox", `0 0 1000 500`)
-          .attr("preserveAspectRatio", "xMinYMin meet")
-        // .attr("width", "100%")
-        // .attr("height", "100%")
+    .attr("viewBox", `0 0 1000 500`)
+    .attr("preserveAspectRatio", "xMinYMin meet")
     .append("g")
-        .attr("transform", 
-              "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var lineDuration = d3.select("body .page04 .case_line").append("div")
 .attr("class", "line_duration")
 .html("<p>통계기간: 2020년 01월 20일 ~ 2021년 08월 02일<br>(출처: 질병관리청)</p>")
+
 
 d3.csv("./content/case_data.csv", function(error, data) {
     data.forEach(function(d) {
@@ -347,17 +337,17 @@ d3.csv("./content/case_data.csv", function(error, data) {
     y.domain([0, d3.max(data, function(d) { return d.case; })]);
 
     svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(data));
+       .attr("class", "line")
+       .attr("d", valueline(data));
 
     svg.selectAll("dot")	
-        .data(data)			
-    .enter().append("circle")								
+       .data(data)			
+       .enter().append("circle")								
         .attr("r", 5)		
         .attr("cx", function(d) { return x(d.date); })		 
         .attr("cy", function(d) { return y(d.case); })		
         .on("mouseover", function(d) {		
-          lineTooltip	.html("<p>"+formatTime(d.date)+"</p>" + "<p>"+d.case+"명</p>")	
+          lineTooltip	.html("<p>"+formatTime(d.date)+"</p>" + "<p>"+d3.format(',')(d.case)+"명</p>")	
                 .style("left", (d3.event.pageX- 50) + "px")		
                 .style("top", (d3.event.pageY- 75) + "px")
                 .style("opacity", 1);	
@@ -366,13 +356,11 @@ d3.csv("./content/case_data.csv", function(error, data) {
           lineTooltip.style("opacity", 0);	
         });
 
-    // Add the X Axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    // Add the Y Axis
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
@@ -380,7 +368,7 @@ d3.csv("./content/case_data.csv", function(error, data) {
 });
 
 
-// /////page05//////
+//--------------page05--------------
 gsap.from('.page05 > .title:nth-child(1) > h1', {
   y:100,
   opacity: 0,
@@ -406,7 +394,8 @@ gsap.from('.page05 > .title:nth-child(3) > h1', {
   }
 })
 
-// ///world_vaccine////
+// //국가별 백신 접종률 세계 지도//
+
 const worldMapWidth = 1000;
 const worldMapHeight = 700; // svg 크기 설정
 
@@ -432,57 +421,26 @@ var worldMapTooltip = d3.select(".page05 .world_vaccine").append("div")
 .style("opacity", 0);
 
 
-        d3.json("./content/worldmap_topo.json", function(error, data) {
+  d3.json("./content/worldmap_topo.json", function(error, data) {
 
           var features = topojson.feature(data, data.objects.worldmap).features;
           
           worldMapSvg.attr("viewBox", `0 0 ${worldMapWidth} ${worldMapHeight}`)
-          // worldMapSvg.attr("width", worldMapWidth)
-          // .attr("height", worldMapHeight) // 맨 위에서 설정한 값으로 svg 크기값 전달
-          .append("g"); //svg 그룹화
       
-      worldMapSvg.selectAll("path") //지도 그릴 선들을 모두 선택
-          .data(features) //연결된 world.js 파일에서 선언한 변수 worldMap의 features값들을 가져옴
-          .enter()
-          .append("path") //새로운 값이 추가되면 또 path로 그린다
-          .attr("d", worldMapPath) // 여기서 d가 뭘까?
-          .attr("class", "country") // 각각의 path들에 country라는 class값을 준다.
-          .on("mouseover", function(d) {		
-            worldMapTooltip	.html(`<p>${d.properties.name}</p><p>${d.properties.vaccinated}%</p>`)
-              .style("left", (d3.event.pageX) + "px")		
-              .style("top", (d3.event.pageY - 28) + "px")
-              .style("opacity", 1)	
-          })					
-          .on("mouseout", function(d) {		
-            worldMapTooltip.style("opacity", 0);	
-              });
-      
-      
-      // worldMapSvg.selectAll("text")
-      // .data(features)
-      // .enter().append("text")
-      // .attr("transform", function(d) { return "translate(" + worldMapPath.centroid(d) + ")"; })
-      // .attr("dy", ".35em")
-      // .attr("dx", "-.6em")
-      // .attr("class", (function(d) { return "country-name "+d.properties.iso_a3; }))
-      // // .text(function(d) { return d.properties.iso_a3; })
-      // .text(function(d){
-      //             if (d.properties.vaccinated !== undefined){
-      //                 return d.properties.iso_a3;
-      //             }
-      //         })
+          worldMapSvg.selectAll("path") 
+              .data(features) 
+              .enter()
+              .append("path") 
+              .attr("d", worldMapPath) 
+              .attr("class", "country") 
+
+      newFeatures = features.filter( function(d){return d.properties.vaccinated !==undefined} )
 
               worldMapSvg.selectAll("circle")
-              .data(features)
+              .data(newFeatures)
               .enter().append("circle")
               .attr("transform", function(d) { return "translate(" + worldMapPath.centroid(d) + ")"; })
-              .attr("r", function(d){
-                  if (d.properties.vaccinated !== undefined){
-                              return 10;
-                          }else{
-                              return 0;
-                          }
-              })
+              .attr("r", 10)
               .attr("class", (function(d) { return "country-name "+d.properties.iso_a3; }))
               .style("fill", "red")
               .on("mouseover", function(d) {		
@@ -499,10 +457,7 @@ var worldMapTooltip = d3.select(".page05 .world_vaccine").append("div")
 
 
 
-
-
-
-// ///korea_vaccine////
+// //한국 연령대별 백신 접종률 막대 그래프//
 
 var korVaccineData = [
   {x: 77.1, y: "80세 이상", z:1738733},
@@ -516,7 +471,6 @@ var korVaccineData = [
 
 var korVWidth = 1000;
 var korVHeight = 700;
-  // var color = ['red','blue','yellow','green']
 
   var korVDsc= d3.select("body .page05 .korea_vaccine").append("div")
     .attr("class", "korV_dsc")
@@ -525,9 +479,8 @@ var korVHeight = 700;
   var korVsvg = d3.select(".page05 .korea_vaccine")
   .append("svg")
   .attr("viewBox", `0 0 ${korVWidth} ${korVHeight}`)
-  // .style("min-height", 700)
-  .append("g") //svg전체를 그룹화시킨다, 이거 안쓰면 axis(축)은 안움직임
-  .attr("transform", "translate(0,20)"); //svg위치를 전체 옮긴다.
+  .append("g") 
+  .attr("transform", "translate(0,20)"); 
 
   // var korVSrc= d3.select("body .page05 .korea_vaccine").append("div")
   // .attr("class", "korV_src")
@@ -544,16 +497,15 @@ var korVHeight = 700;
   var svgG = korVsvg.append("g")
       .attr("transform", "translate(130,0)");
 
-  
   svgG.selectAll('rect')
   .data(korVaccineData)
-  .enter() //부족한 선택물은 추가 계산 | enter(): 부족한 선택물을 추가하는 ㄴ메서드
+  .enter() 
   .append('rect')
       .attr('height', 50)
-      .attr('y', (d,i)=>korVyScale(d.y)) // y축에 data들을 70만큼 거리를 둔다
-      .attr('width', 10) //width 기본값
+      .attr('y', (d,i)=>korVyScale(d.y))
+      .attr('width', 10) 
       .on("mouseover", function(d) {		
-        korVTooltip	.html("<p>접종완료자: "+d.z+"명</p>" +"<p>접종률: "+ d.x+"%</p>")	
+        korVTooltip	.html("<p>접종완료자: <strong>"+d3.format(",")(d.z)+"</strong>명</p>" +"<p>접종률: <strong>"+ d.x+"</strong>%</p>")	
               .style("left", (d3.event.pageX) + "px")		
               .style("top", (d3.event.pageY - 10) + "px")
               .style("opacity", 1);	
@@ -561,15 +513,8 @@ var korVHeight = 700;
       .on("mouseout", function(d) {		
         korVTooltip.style("opacity", 0);	
       })
-      // .on("mouseover", function() {korVcTooltip.style("display", "block");})
-      // .on("mouseout", function() {korVcTooltip.style("display", "none");})
-      // .on("mousemove", function(d){
-      //   korVcTooltip.style("left", (d3.event.pageX + 10) +"px");
-      //   korVcTooltip.style("top", (d3.event.pageY -10) + "px");
-      //   korVcTooltip.html("<p>접종완료자: "+d.z+"명</p>" +"<p>접종률: "+ d.x+"%</p>");
-      // })
-      .transition().duration(1500) //추가된 선택물을 움직여준다.
-      .attr('width', d=>(d.x*8.2)) //svg의 가로길이만큼 곱해주고 100으로 나눠 백분율로 나타나게함
+      .transition().duration(1500) 
+      .attr('width', d=>(d.x*8.2)) 
       
 
   var yAxis2 = d3.axisLeft().scale(korVyScale);
@@ -582,9 +527,6 @@ var korVHeight = 700;
       .attr("transform", "translate(0,"+(height+135)+")")
       .call(xAxis2);
 
-  // svgG.append("image")
-  //     .attr("xlink")
-
   var korVTooltip = d3.select("body .page05 .korea_vaccine").append("div")
   .attr("class", "korV_tooltip")
   .style("opacity", 0);
@@ -592,7 +534,7 @@ var korVHeight = 700;
 
 
 
-// /////////page06
+//--------------page06--------------
 
 gsap.from('.page06 > .title > h1', {
   y:100,
@@ -614,10 +556,6 @@ korMapHeight = 1000;
 var korMapSvg = d3.select(".page06 .korea_case").append("svg")
 .attr("viewBox", `0 0 ${korMapWidth} ${korMapHeight}`)
 .style("min-width", 500)
-// .attr("width", korMapWidth)
-// .attr("height", korMapHeight);
-
-// var korMap = korMapSvg.append("g").attr("id", "map");
 
 var korMapProjection = d3.geoMercator()
 .center([127.2895, 37.4651])
@@ -633,73 +571,72 @@ var korMapTooltip = d3.select(".page06 .korea_case").append("div")
 
 d3.json("./content/skorea_provinces_topo_simple.json", function(error, data) {
 
-var features = topojson.feature(data, data.objects.skorea_provinces_geo).features;
+    var features = topojson.feature(data, data.objects.skorea_provinces_geo).features;
 
-var quantize = d3.scaleQuantize()
-.domain([0,10000])
-.range(d3.range(9).map(function(i){return i ;}))
+    var quantize = d3.scaleQuantize()
+    .domain([0,10000])
+    .range(d3.range(9).map(function(i){ return i ;}))
 
-features.forEach(function(d){
-d.properties.quantized = quantize(d.properties.case);
-})
+    features.forEach(function(d){
+    d.properties.quantized = quantize(d.properties.case);
+    })
 
-korMapSvg.selectAll("path")
-.data(features)
-.enter().append("path")
-.attr("class", function(d) { return "city c" + d.properties.quantized; })
-.attr("d", korMapPath)
-.on("mouseover", function(d) {		
-  korMapTooltip	.html(`<p>${d.properties.name}</p><p>${d.properties.case}</p><p>(+${d.properties.new})</p>`)
-    .style("left", (d3.event.pageX) + "px")		
-    .style("top", (d3.event.pageY - 28) + "px")
-    .style("opacity", 1)	
-})					
-.on("mouseout", function(d) {		
-  korMapTooltip.style("opacity", 0);	
-});
+    korMapSvg.selectAll("path")
+    .data(features)
+    .enter().append("path")
+    .attr("class", function(d) { return "city c" + d.properties.quantized; })
+    .attr("d", korMapPath)
+    .on("mouseover", function(d) {		
+      korMapTooltip	.html(`<p>${d.properties.name}</p><p>${d3.format(",")(d.properties.case)}</p><p>(+${d.properties.new})</p>`)
+        .style("left", (d3.event.pageX) + "px")		
+        .style("top", (d3.event.pageY - 28) + "px")
+        .style("opacity", 1)	
+    })					
+    .on("mouseout", function(d) {		
+      korMapTooltip.style("opacity", 0);	
+    });
 
-korMapSvg.selectAll("text")
-.data(features)
-.enter().append("text")
-.attr("transform", function(d) { return "translate(" + korMapPath.centroid(d) + ")"; })
-.attr("dx", "-.6em")
-.attr("dy", function(d){
-  if(d.properties.code == 31){
-    return "-3em"
-  }else if(d.properties.code == 33){
-    return "-2em"
-  }else{
-    return ".55em"
-  }
-})
-.style("fill", function(d){
-  if(d.properties.code == 23 || d.properties.code == 21){
-    return "white"
-  }else{
-    return "black"
-  }
-})
-.attr("class", (function(d) { return "city-label "+d.properties.code; }))
-.text(function(d) { return d.properties.name; })
-.on("mouseover", function(d) {		
-  korMapTooltip	.html(`<p>${d.properties.name}</p><p>${d.properties.case}</p><p>(+${d.properties.new})</p>`)
-    .style("left", (d3.event.pageX) + "px")		
-    .style("top", (d3.event.pageY - 28) + "px")
-    .style("opacity", 1)
-})					
-.on("mouseout", function(d) {		
-  korMapTooltip.style("opacity", 0);	
-});
-
-
+    korMapSvg.selectAll("text")
+    .data(features)
+    .enter().append("text")
+    .attr("transform", function(d) { return "translate(" + korMapPath.centroid(d) + ")"; })
+    .attr("dx", "-.6em")
+    .attr("dy", function(d){
+      if(d.properties.code == 31){
+        return "-3em"
+      }else if(d.properties.code == 33){
+        return "-2em"
+      }else{
+        return ".55em"
+      }
+    })
+    .style("fill", function(d){
+      if(d.properties.code == 23 || d.properties.code == 21){
+        return "white"
+      }else{
+        return "black"
+      }
+    })
+    .attr("class", (function(d) { return "city-label "+d.properties.code; }))
+    .text(function(d) { return d.properties.name; })
+    .on("mouseover", function(d) {		
+      korMapTooltip	.html(`<p>${d.properties.name}</p><p>${d3.format(",")(d.properties.case)}</p><p>(+${d.properties.new})</p>`)
+        .style("left", (d3.event.pageX) + "px")		
+        .style("top", (d3.event.pageY - 28) + "px")
+        .style("opacity", 1)
+    })					
+    .on("mouseout", function(d) {		
+      korMapTooltip.style("opacity", 0);	
+    });
 
 });
 
-d3.selectAll(".municipality-label")
-.attr("dy", ".5em");
+// d3.selectAll(".municipality-label")
+// .attr("dy", ".5em");
 
 
-// ////page07
+//--------------page07--------------
+
 const tlL = gsap.timeline();
 tlL.from('.page07 > .title > div',  {
   y:100,
@@ -709,8 +646,8 @@ tlL.from('.page07 > .title > div',  {
       trigger: '.page07 > div',
       scrub: 1,
       toggleActions: 'play reverse none reverse',
-      start: 'top 50%',
-      end: "+=700",
+      start: 'top 100%',
+      end: "+=500",
   }
 })
 .to('.page07 > .bg',  {
